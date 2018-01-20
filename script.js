2479eb21c9ae3a5af2cd9507e3751c920701c1c3
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-01-20 16:49:11 +0530

    made desktop version, functional timer

--- script.js
+++ script.js
@@ -10,5 +10,38 @@ function init()
   close_list.onclick = function() {
     list.style.top = '100%';
   };
+
+  time = document.getElementsByClassName('time')[0];
+  time_bar = document.getElementsByClassName('time-bar')[0];
+  time.innerHTML='00:00';
+  quiz_len = 60;
+  window.onload = function() {
+  	min = 0;
+  	sec = 0;
+  	setInterval(function() {
+  		if(sec==59)
+  		{
+  			if(min==quiz_len-1)
+  			{
+  				return;
+  			}
+  			sec=-1;
+  			min++;
+  		}
+  		sec++;
+  		sec = sec.toString();
+  		min = min.toString();
+  		if(sec.length<2)
+  			sec='0'+sec;
+  		if(min.length<2)
+  			min='0'+min;
+  		time.innerHTML=min+':'+sec;
+  		sec=parseInt(sec);
+  		min=parseInt(min);
+
+  		//change length of time bar
+  		time_bar.style.width = (min+sec/60)/quiz_len * 100 + '%';
+  	}, 1000);
+  };
 }
 init();
